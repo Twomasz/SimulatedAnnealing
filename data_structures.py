38829ae8ed31bfde45c30b5703ade6f0fdefb4f4
Twomasz <tomasz.wojcick@gmail.com>
2022-12-13 03:11:30 +0100

    failed try to solve data structured problems

--- data_structures.py
+++ data_structures.py
@@ -1,5 +1,4 @@
-from typing import List, Tuple, Dict, Union
-
+import random
 """
 N = 0       # liczba części dostępnych w magazynie
 K = 0       # liczba wybranych części do sprzedaży
@@ -12,46 +11,54 @@ m_recz = 0  # ręcznie ustawiana składowa marży przez kierownika
 """
 
 
+
 class Item:
     """
     Klasa przechowująca informacje o i-tym przedmiocie w magazynie.
     Jej atrybutami są: nazwa, cena, marża. Oraz informacje rynkowe
     na temat przedmiotu takie jak: średnia cen, ilość aukcji.
-    Pierwotnie dodajemy naszą część, a następnie po researchu
-    jestesmy w stanie zaktualizować marżę oraz informacje rynkowe
+    Pierwotnie dodajemy naszą część, a następnie po research-u
+    jesteśmy w stanie zaktualizować marżę oraz informacje rynkowe
     niezależnie
     """
 
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
-        self.margin = None
-        self.avg_of_prices = None
-        self.quantity_of_auctions = None
+        self.margin = 0
+        self.avg_of_prices = price*price
+        self.quantity_of_other_auctions = 10
 
-    def update_margin(self, margin):
-        self.margin = margin
+    # def update_margin(self, margin):
+    #     self.margin = margin
 
-    def update_market_info(self, avg_of_prices, quantity_of_auctions):
+    def update_market_info(self, avg_of_prices, quantity_of_other_auctions):
         self.avg_of_prices = avg_of_prices
-        self.quantity_of_auctions = quantity_of_auctions
+        self.quantity_of_other_auctions = quantity_of_other_auctions
+
+    def __str__(self):
+        return f'{self.name}: {self.price}, ({self.margin:.2f}%)'
 
 
 class Warehouse:
     """
     Klasa przechowująca informacje o zawartości magazynu
     zawartość magazynu jest listą obiektów Item, po
-    dodaniu pierwotnej, można dodać pojedyńcze za
+    dodaniu pierwotnej, można dodać pojedyncze za
     pomocą metody add_item
     """
 
     def __init__(self, stored_items: list):
         self.stored_items = stored_items
-        self.N = len(self.stored_items)
 
     def add_item(self, item: Item):
         self.stored_items.append(item)
-        self.N = len(self.stored_items)
+
+    def __str__(self):
+        string = ''
+        for item in self.stored_items:
+            string += f'{item}' + ', '
+        return string
 
 
 class Company:
@@ -59,66 +66,75 @@ class Company:
     To reprezentacja naszej firmy i czynności, jakie należy wykonać, by dojść do rozwiązania
     """
 
-    def __init__(self, K, B, m_manual):
-        self.K = K
-        self.B = B
-        self.m_manual = m_manual
+    def __init__(self, quantity_of_items_to_sell, budget, manual_margin):
+        # TODO: zabezpieczenie przed wybraniem K > B
+        self.quantity_of_items_to_sell = quantity_of_items_to_sell
+        self.budget = budget
+        self.m_manual = manual_margin
 
-    def update_margins_from_warehouse(self, warehouse: Warehouse,
-                                   item: Item):  # metoda do obliczania marż wszystkich przedmiotów
-        items_margins = dict()
+    def update_margins_from_warehouse(self, warehouse: Warehouse):  # metoda do obliczania marż wszystkich przedmiotów
+        for item in warehouse.stored_items:
+            avg_of_prices, quantity_of_auctions = item.avg_of_prices, item.quantity_of_other_auctions
 
-        avg_of_prices = item.avg_of_prices
-        quantity_of_auctions = item.quantity_of_auctions
-
-        for i in range(warehouse.N):
-            item = warehouse.stored_items[i]
-            avg_of_prices, quantity_of_auctions = warehouse.stored_items[item.name]
-
-            ingredient1 = (avg_of_prices - item.price) / item.price
-
-            ingredient2 = quantity_of_auctions / 50
-
-            # TODO: wymyslic zalezność kupowanej ilości od zmiejszającego się popytu
-
-            items_margins[item.name] = self.m_manual + ingredient1 + ingredient2
-
-        return items_margins
+            item.margin = self.m_manual + (avg_of_prices - item.price) / item.price + quantity_of_auctions / 50
 
 
 class Solution:
-    def __init__(self, elems, K, type: str):
-        self.elems = elems
-        self.K = K
-        self.solution = self.__find_solution(type)
+    def __init__(self, company: Company, stored_items: list, type: str ='prob', type2: str ='1'):
+        self.company = company
+        self.stored_items = stored_items
+        self.solution = {'indexes': None, 'quantity': None}
+        self.__find_first_solution(type)
 
-    def __eq__(self, other):
-        pass
+    def __gt__(self, other):
+        return self.solution > other.solution
 
-    def __find_solution(self, type):
+    def __lt__(self, other):
+        return self.solution < other.solution
+
+    def __find_first_solution(self, type='prob'):
         if type == 'prob':
-            pass
             # random
+            elems_idx = [i for i in range(len(self.stored_items))]
+            self.solution['indexes'] = random.choices(elems_idx, k=self.company.quantity_of_items_to_sell)
+
         elif type == 'smallest':
             pass
             # najmniejsze
         elif type == 'greatest':
             pass
-            # najwieksze
+            # największe
         else:
-            raise ValueError('Nieprawidłowy typ znalezienia rozwiązania początkowego')
+            raise ValueError('Nieprawidłowy typ rozwiązania początkowego')
 
     def repair_solution(self):
-        pass
+        if self.not_in_budget():
 
-    def is_in_budget(self, budget):
+
+            # rekurencja
+            self.repair_solution()
+
+    def not_in_budget(self):
         total_price = 0
 
-        if total_price <= budget:
+        if total_price > self.company.budget:  # rozwiązanie nie mieści się w budżecie
             return True
         else:
             return False
 
     # TODO: dodać metody porównania obiektów rozwiązań i przekazanie 75% części do kolejnego obiektu
+
+    def __find_adjacency_solution(self, type2='1'):
+        if type2 == '1':
+            pass
+
+        elif type2 == '2':
+            pass
+
+        elif type2 == '3':
+
+            return Solution(self.company, self.stored_items)
+        else:
+            raise ValueError('Nieprawidłowy typ definicji sąsiedztwa')
 
 
--- tests.py
+++ tests.py
@@ -1,12 +1,18 @@
-from data_structures import Item, Warehouse
+from data_structures import *
 
 
 # 5 przykładowych częsci oraz przykładowy obiekt Warehouse
 
 stored_items = [Item('Wahacz przedni BMW E46', 100), Item('Wahacz tylni BMW E46', 150),
-               Item('Klocki hamulcowe BMW E46', 190), Item('Dyferencjal BMW E46', 300), Item('Katalizator BMW E46', 610)]
+                Item('Klocki hamulcowe BMW E46', 190), Item('Dyferencjał BMW E46', 300),
+                Item('Katalizator BMW E46', 610)]
 
 
 warehouse = Warehouse(stored_items)
 print(warehouse)
 
+company = Company(4, 1000, 0.1)
+
+company.update_margins_from_warehouse(warehouse)
+
+print(warehouse)
